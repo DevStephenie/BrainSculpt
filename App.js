2915c878d7bce1fcd7895e4ff4a04567f052b26b
leqals <saateranongu1@gmail.com>
2023-07-11 23:06:56 +0100

    Implement feature X

--- App.js
+++ App.js
@@ -2,13 +2,12 @@
 // import { firebaseConfig } from './config/firebaseConfig'; // Your Firebase configuration
 // Initialize Firebase
 // initializeApp(firebaseConfig);
+// import { useFonts } from "expo-font";
 
 import React from "react";
-import { Text } from "react-native";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeWindStyleSheet } from "nativewind";
-import { useFonts } from "expo-font";
 import store from "./src/reducers/store";
 import { AuthNavigator } from "./src/navigations";
 
@@ -17,14 +16,11 @@ NativeWindStyleSheet.setOutput({
 });
 
 const App = () => {
-  const [loaded] = useFonts({
-    openSans: require("./src/assets/font/openSans.ttf"),
-  });
 
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <AuthNavigator style={{ fontFamily: "openSans" }} />
+        <AuthNavigator  />
       </NavigationContainer>
     </Provider>
   );
